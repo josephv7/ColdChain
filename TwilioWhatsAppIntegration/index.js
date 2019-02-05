47f2ee561033a404128a95507a6c2407388dd556
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-02-06 00:22:49 +0530

    Sending message using whatsapp working

--- TwilioWhatsAppIntegration/index.js
+++ TwilioWhatsAppIntegration/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
+const accountSid = 'ACf5566ab9e38d13007b998a1296b19516';
+const authToken = 'api_token_here';
+const client = require('twilio')(accountSid, authToken);
+
 app.get('/', function (req, res) {
   console.log('Hello World!');
+
+  client.messages
+        .create({
+          body: 'Hello there!',
+          from: 'whatsapp:+14155238886',
+          to: 'whatsapp:+918289940688'
+        })
+        .then(message => console.log(message.sid))
+        .done();
+
+
+
 });
 
 app.listen(5000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 5000!');
 });
