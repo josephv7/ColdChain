c86657509c6ecd5fe3f014861202f81a74979f06
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-02-05 23:23:31 +0530

    POST request and blockchain updation working

--- cold-chain-backend/index.js
+++ cold-chain-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const url = require('url');
 const querystring = require('querystring');
+const Request = require("request");
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +13,24 @@ app.get('/', async function(req, res) {
 console.log('inside get method');
 console.log(req.query.temperature);
 console.log(req.query.location);
+
+
+Request.post({
+    "headers": { "content-type": "application/json" },
+    "url": "http://localhost:3000/api/TemperatureDrop",
+    "body": JSON.stringify({
+        "asset": "org.example.mynetwork.MedicinePackage#0002",
+        "newTemperature": String(req.query.temperature),
+        "newLocation": String(req.query.location)
+    })
+}, (error, response, body) => {
+    if(error) {
+        return console.dir(error);
+    }
+    console.dir(JSON.parse(body));
+});
+
+
 });
 
   let server = app.listen(4000, function() {
