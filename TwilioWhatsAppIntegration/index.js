3a6a623036f727de40b3baee27e1daaee60d1f88
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-02-08 15:34:25 +0530

    second get api created

--- TwilioWhatsAppIntegration/index.js
+++ TwilioWhatsAppIntegration/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 
-const accountSid = 'ACf5566ab9e38d13007b998a1296b19516';
-const authToken = 'api_token_here';
+const accountSid = 'id_here';
+const authToken = 'token_here';
 const client = require('twilio')(accountSid, authToken);
 
-app.get('/', function (req, res) {
-  console.log('Hello World!');
+app.get('/temperature', function (req, res) {
+  console.log('Temperature Change');
 
   client.messages
         .create({
-          body: 'Hello there!',
+          body: 'Tampered!',
           from: 'whatsapp:+14155238886',
           to: 'whatsapp:+918289940688'
         })
@@ -20,6 +20,25 @@ app.get('/', function (req, res) {
 
 
 });
+
+
+app.get('/location', function (req, res) {
+  console.log('Location Change');
+
+  client.messages
+        .create({
+          body: 'Location changed!',
+          from: 'whatsapp:+14155238886',
+          to: 'whatsapp:+918289940688'
+        })
+        .then(message => console.log(message.sid))
+        .done();
+
+
+
+});
+
+
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
