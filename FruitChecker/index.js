d38c31b75a6967d422b4bffbe19d9bea37116f9a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-02-11 20:29:38 +0530

    temp push...error in filename for ipfs

--- FruitChecker/index.js
+++ FruitChecker/index.js
@@ -5,6 +5,7 @@ const url = require('url');
 const querystring = require('querystring');
 const Request = require("request");
 const rgbToHsl = require('rgb-to-hsl');
+const IPFS = require('ipfs')
 
 
 let app = express();
@@ -12,10 +13,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+
+
 // function getHSL(){
 // console.log(rgbToHsl(255, 192, 203));
 // return rgbToHsl(255, 192, 203);
 // }
+
+app.get('/ipfs',async function(req,res){
+
+console.log('inside ipfs');
+console.log(req.query.red);
+console.log(req.query.green);
+
+
+const node = new IPFS()
+node.on('ready', async () => {
+    console.log('iniside sample code')
+    const version = await node.version()
+  
+    console.log('Version:', version.version)
+  
+    const filesAdded = await node.add({
+        // path: 'hello.txt',
+    // content: Buffer.from('Hello World 101222244')
+      path: Date.now().toString,
+      content: Buffer.from('Red Count : ' + req.query.red + ' & Green Count : ' + req.query.green + '. Timestamp : ' + Date.now().toString())
+    })
+  
+    console.log('Added file:', filesAdded[0].path, filesAdded[0].hash)
+
+
+    try {
+        await node.stop()
+        console.log('Node stopped!')
+      } catch (error) {
+        console.error('Node failed to stop cleanly!', error)
+      }
+
+
+  })
+  
+
+
+
+
+
+
+
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+})
 
 
 app.get('/gethsl', async function(req, res) {
@@ -46,6 +97,6 @@ if(Math.round(hsl[0]) > 0 && Math.round(hsl[0]) < 65 ){
 })
 
 
-let server = app.listen(4001, function() {
-    console.log('Server is listening on port 4001')
+let server = app.listen(5001, function() {
+    console.log('Server is listening on port 5001')
 });
