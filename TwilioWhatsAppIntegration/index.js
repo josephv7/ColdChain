d893bb760e0226ff80fbaa0981d1597a72bbb444
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-02-12 00:12:48 +0530

    Added new API for destination reached, fetching query items from url, need to update message body

--- TwilioWhatsAppIntegration/index.js
+++ TwilioWhatsAppIntegration/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var app = express();
 
-const accountSid = 'id_here';
-const authToken = 'token_here';
+const accountSid = 'sid';
+const authToken = 'token';
 const client = require('twilio')(accountSid, authToken);
 
 app.get('/temperature', function (req, res) {
   console.log('Temperature Change');
+  console.log(req.query.temperature)
+  console.log(req.query.location)
 
   client.messages
         .create({
-          body: 'Tampered!',
+          body: 'Tampered!' + req.query.temperature,
           from: 'whatsapp:+14155238886',
           to: 'whatsapp:+918289940688'
         })
@@ -24,10 +26,31 @@ app.get('/temperature', function (req, res) {
 
 app.get('/location', function (req, res) {
   console.log('Location Change');
+  console.log(req.query.temperature)
+  console.log(req.query.location)
 
   client.messages
         .create({
           body: 'Location changed!',
+          from: 'whatsapp:+14155238886',
+          to: 'whatsapp:+918289940688'
+        })
+        .then(message => console.log(message.sid))
+        .done();
+
+
+
+});
+
+
+pp.get('/destination', function (req, res) {
+  console.log('Location Change');
+  console.log(req.query.temperature)
+  console.log(req.query.location)
+
+  client.messages
+        .create({
+          body: 'Destination Reached',
           from: 'whatsapp:+14155238886',
           to: 'whatsapp:+918289940688'
         })
